c998650a27a87573706ad5bc00626beffa89f949
cousim46 <wjdghldns24@naver.com>
2026-01-13 21:49:21 +0900

    feat : 익스체인지와 큐 연결하는 방법

---
+++ src/main/java/study/rabbitmq/ExchangeQueueConnection.java
@@ -0,0 +1,28 @@
+package study.rabbitmq;
+
+import com.rabbitmq.client.AMQP.Exchange;
+import com.rabbitmq.client.AMQP.Queue;
+import com.rabbitmq.client.AMQP.Queue.BindOk;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+public class ExchangeQueueConnection {
+    private static final String QUEUE_NAME = "queueName";
+    private static final String EXCHANGE_NAME = "exchangeName";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("127.0.0.1");
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+        Queue.DeclareOk queueDeclareOk = channel.queueDeclare(QUEUE_NAME, false,false,false, Map.of());//큐 선언 및 생성
+        Exchange.DeclareOk exchangeDeclareOk = channel.exchangeDeclare(EXCHANGE_NAME,
+            BuiltinExchangeType.DIRECT);//익스체인지 선언 및 생성
+        BindOk queueBindOk = channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, "routingKey");
+    }
+}
