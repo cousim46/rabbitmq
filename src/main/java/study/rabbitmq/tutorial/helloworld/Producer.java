1869305f16d8f15b6528742d59991ab30e193bf7
cousim46 <wjdghldns24@naver.com>
2026-02-24 23:13:37 +0900

    feat : RabbitMQ 튜토리얼 - 생산자 생성 및 메시지 발행

---
+++ src/main/java/study/rabbitmq/tutorial/helloworld/Producer.java
@@ -0,0 +1,27 @@
+package study.rabbitmq.tutorial.helloworld;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import study.rabbitmq.tutorial.config.RabbitMqConfig;
+/**
+ * 브로커가 충분한 디스크 여유 공간(기본적으로 최소 50MB의 여유 공간 필요)없이 시작되어 메시지를 수신하지 못할 수 있음.
+ *
+ * */
+public class Producer {
+
+    private static final String QUEUE_NAME = "queue";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory connectionFactory = RabbitMqConfig.connectionFactory();
+        try (Connection connection = connectionFactory.newConnection();
+            Channel channel = connection.createChannel();) {
+            // 큐 선언, 큐가 존재하지 않는 경우에만 생성
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println("Send message = " + message);
+        }
+    }
+
+}
